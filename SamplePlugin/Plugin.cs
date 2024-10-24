66c963f9484f0a25e2352e141cb6f107cb8086a4
Rafael Dalcolmo <9065913+RDalcolmo@users.noreply.github.com>
2024-10-23 20:55:30 -0400

    Changed command name

--- SamplePlugin/Plugin.cs
+++ SamplePlugin/Plugin.cs
@@ -14,7 +14,7 @@ public sealed class Plugin : IDalamudPlugin
     [PluginService] internal static ITextureProvider TextureProvider { get; private set; } = null!;
     [PluginService] internal static ICommandManager CommandManager { get; private set; } = null!;
 
-    private const string CommandName = "/goldbuddy";
+    private const string CommandName = "/bansync";
 
     public Configuration Configuration { get; init; }
 
@@ -37,7 +37,7 @@ public sealed class Plugin : IDalamudPlugin
 
         CommandManager.AddHandler(CommandName, new CommandInfo(OnCommand)
         {
-            HelpMessage = "Opens up GoldBuddy's alarm interface"
+            HelpMessage = "Opens up BanSync's interface"
         });
 
         PluginInterface.UiBuilder.Draw += DrawUI;
